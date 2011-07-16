fba9531ac3df582b531bb9e686d4c31c2cca6ea2
Darklg <kev.rocher@gmail.com>
2011-07-16 15:52:24 +0200

    Debut du plugin
    - Ajout du fichier de base du plugin
    - Ajout d'un répertoire protégé pour les inclusions

---
+++ dk_bwp_base_plugin/dk_bwp_base_plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: DK Base WordPress Plugin
+Plugin URI: http://github.com/Darklg/BaseWordpressPlugin
+Description: Framework de plugin Wordpress
+Author: Darklg
+Version: 1
+Author URI: http://darklg.me
+*/
