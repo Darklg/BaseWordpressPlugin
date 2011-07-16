58dfa470c549ac4d3829c8e01bbcc3d9948f08a1
Darklg <kev.rocher@gmail.com>
2011-07-16 16:07:32 +0200

    Constantes de base et Fonctions d'activation/desactivation

---
+++ dk_bwp_base_plugin/includes/active_inactive.php
@@ -0,0 +1,13 @@
+<?php
+
+// Activation du plugin
+register_activation_hook(dk_bwp_file, 'dk_bwp_activation');
+function dk_bwp_activation() {
+    // Action
+}
+
+// Desactivation du plugin
+register_deactivation_hook(dk_bwp_file, 'dk_bwp_deactivation');
+function dk_bwp_deactivation() {
+    // Action
+}
